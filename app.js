423e2b018d03d9cf5a299822497e26622dfcc7a7
G Chaithanya Kumar <20551a0220.chaitanya@gmail.com>
2023-03-03 21:48:39 +0530

    ALL DONE..TO-Do list webapp completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
-var NEWITEMS = []
+var NEWITEMS = ["Complete Work Fast", "Eat Less Food", "Exercise Daily"]
 
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
@@ -14,11 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
 
-    res.render("lists", { name: NEWITEMS })        ///accessing the var data which is at top of the app bcoz render method accepts only in the route data.
+    res.render("lists", { name: NEWITEMS, heading: "TO-DO LIST" })    ///accessing the var data which is at top of the app bcoz render method accepts only in the route data.
 })
 
 
 app.post("/", (req, res) => {
+      
     var NEWITEM = req.body.newItem               //getting post request data
     NEWITEMS.push(NEWITEM)                //sending post data to var at top of app
     res.redirect("/")               //redirecting to homepage after post request sent
